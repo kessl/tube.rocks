e0c9b409467de6522b177852a2f17ed618521967
Daniel Kessl <dan@kessl.net>
2023-08-21 20:53:51 +0300

    Design new mix form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def yt_video_id(url)
-    CGI::parse(URI::parse(url.presence || '').query || '')['v'].first
-  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,12 @@
 class Video < ApplicationRecord
+  include ActionView::Helpers
+
   belongs_to :mix
 
   validates :url, presence: true
   validates :yt_video_id, presence: { message: 'Please provide a valid URL to a YouTube video' }
 
   before_validation do
-    self.yt_video_id = yt_video_id(url)
+    self.yt_video_id = CGI::parse(URI::parse(url).query || '')['v'].first
   end
 end
