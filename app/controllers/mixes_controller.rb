b2f215264fb31aaeae1cb0494bc75aea4cbc40e2
Daniel Kessl <dan@kessl.net>
2024-07-05 22:11:45 +0200

    Fix new mix form

--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -8,12 +8,14 @@ class MixesController < ApplicationController
   end
 
   def new
-    base_mix = Mix.includes(:videos).find_by(slug: params[:slug])
+    base_mix = Mix.includes(:videos).find_by(slug: params[:from])
     if base_mix.present?
       @mix = Mix.new({ name: base_mix.name,
                                 videos_attributes: base_mix.videos.map { { url: _1.url, volume: _1.volume } } })
     else
       @mix = Mix.new
+      @mix.videos.build
+      @mix.videos.build
     end
   end
 
@@ -23,9 +25,7 @@ class MixesController < ApplicationController
       @mix = Mix.create!({ name: base_mix.name,
                            videos_attributes: base_mix.videos.map { { url: _1.url, volume: _1.volume } } })
     else
-      @mix = Mix.create!
-      @mix.videos.build
-      @mix.videos.build
+      @mix = Mix.create!(mix_params)
     end
     redirect_to mix_path(slug: @mix.slug)
   end
